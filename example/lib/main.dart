bb430ab246d2d9a662a4f4f9c5e6d4b743881cb1
Hannes Koksch <hannes.koksch@gmail.com>
2023-07-31 13:02:15 +0200

    enhanced example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
+import 'package:example/sketch_mode_switch.dart';
 import 'package:flutter/material.dart';
 import 'package:sketch/sketch.dart';
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 
 void main() {
   runApp(ExampleApp());
@@ -10,7 +14,9 @@ class ExampleApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: SketchPage(),
+    );
   }
 }
 
@@ -21,13 +27,18 @@ class SketchPage extends StatefulWidget {
   State<SketchPage> createState() => _SketchPageState();
 }
 
+final IList<SketchElement> samples = IList([
+  LineEle(Point(50, 50), Point(600, 200), Colors.red, LineType.dotted, 10),
+  LineEle(Point(500, 500), Point(600, 400), Colors.blue, LineType.dotted, 10),
+]);
+
 class _SketchPageState extends State<SketchPage> {
   late SketchController controller;
 
   @override
   void initState() {
     super.initState();
-    controller = SketchController();
+    controller = SketchController(elements: samples);
   }
 
   @override
@@ -36,11 +47,28 @@ class _SketchPageState extends State<SketchPage> {
     super.dispose();
   }
 
+  void _onSelectSketchMode(SketchMode sketchMode) {
+    setState(() {
+      controller.sketchMode = sketchMode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Sketch(
-        controller: controller,
+      body: Stack(
+        children: [
+          Sketch(
+            controller: controller,
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: SketchModeSwitch(
+              sketchMode: controller.sketchMode,
+              onSelectSketchMode: _onSelectSketchMode,
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ example/lib/sketch_mode_switch.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:sketch/sketch.dart';
+
+class SketchModeSwitch extends StatelessWidget {
+  const SketchModeSwitch({
+    Key? key,
+    required this.sketchMode,
+    required this.onSelectSketchMode,
+  }) : super(key: key);
+
+  final SketchMode sketchMode;
+  final Function(SketchMode) onSelectSketchMode;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: SegmentedButton(
+        style: ButtonStyle(),
+        segments: [
+          ButtonSegment(
+            icon: Icon(Icons.edit),
+            label: Text("Edit Mode"),
+            value: SketchMode.edit,
+          ),
+          ButtonSegment(
+            icon: Icon(Icons.line_axis),
+            label: Text("Line Mode"),
+            value: SketchMode.line,
+          ),
+        ],
+        selected: <SketchMode>{sketchMode},
+        onSelectionChanged: (Set<SketchMode> newSelection) {
+          onSelectSketchMode(newSelection.first);
+        },
+      ),
+    );
+  }
+}
